a21c8201c29478309608a61232fb74724394cc29
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-09-03 11:41:05 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,23 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import app from "./app.js";
 import cors from "cors";
-
-app.use(cors());
 dotenv.config();
+app.use(cors({
+  origin: [
+    "https://clouddoc-manager-interface.onrender.com", 
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB Connected"))
-  .catch(err => console.error("❌ MongoDB Error:", err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error("MongoDB Error:", err));
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000, "0.0.0.0", () => {
-  console.log("Server running on http://0.0.0.0:5000");
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
-
